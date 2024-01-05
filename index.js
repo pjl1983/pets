dfa24a8422ef4f673438bdbfd861478033fb6278
Philip Lanphar <philip.lanphar@gmail.com>
2024-01-05 19:13:52 +0100

    added google tag manager

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
+const morgan = require("morgan");
 const path = require('path');
 const app = express();
 const port = 3000;
 
+// set up morgan middleware
+app.use(morgan("combined"));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
-
-  const parseIp = (req) =>
-    req.headers['x-forwarded-for']?.split(',').shift()
-    || req.socket?.remoteAddress
-
-  console.log(`Your IP address is: ${parseIp(req)}`)
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server successfully running on port ${port}`);
