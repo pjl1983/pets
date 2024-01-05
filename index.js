0b7116a2132053d265812ad01cc6a9c6ae9bbd82
Philip Lanphar <philip.lanphar@gmail.com>
2024-01-05 16:04:13 +0100

    Added node.js server

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const hostname = '127.0.0.1';
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,5 +10,5 @@ app.get('/', async(req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server successfully running on port ${port}`);
 });
