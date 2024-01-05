5b1e50912fef8d10e54f12a3589d3e33d4624e48
Philip Lanphar <philip.lanphar@gmail.com>
2024-01-05 18:55:37 +0100

    added google tag manager

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async(req, res) => {
+
+  const parseIp = (req) =>
+    req.headers['x-forwarded-for']?.split(',').shift()
+    || req.socket?.remoteAddress
+
+  console.log(`Your IP address is: ${parseIp(req)}`)
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server successfully running on port ${port}`);
